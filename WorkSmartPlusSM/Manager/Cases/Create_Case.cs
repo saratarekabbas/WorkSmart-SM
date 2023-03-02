908e458c8b7dd343b0ff99dc265e97bb8fe2a258
saratarekabbas <saraatarekk@icloud.com>
2023-03-02 15:09:37 +0800

    Origin: Created NUproject

---
+++ WorkSmartPlusSM/Manager/Cases/Create_Case.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkSmartPlusSM.Manager.Cases
+{
+    internal class Create_Case
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
